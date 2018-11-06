4cd1fe29bb0c00a510594be467c3e3171d26baa9
Jag Gangishetti <jagadeesh.g07@gmail.com>
2018-11-06 12:21:04 -0500

    clear form

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -57,4 +57,9 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     this.subscription.unsubscribe();
   }
 
+  onClear(){
+    this.slForm.reset();
+    this.editMode = false;
+  }
+
 }
